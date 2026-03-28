7ab99f4bf24e325488bb9a980243ed2752fe2652
Hervey67 <herv2007@gmail.com>
2026-03-27 23:08:29 -0700

    Changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,27 +5,22 @@ using namespace std;
 
 int main()
 {
-	int N; // the number of students;
-	int M; // the number of scores;
+	int N, M;
+	cout << "Number of Students and Number of Subjects ";
+	cin >> N >> M;
+		string sname;
+	int score, total = 0;
 
-	cout << "Enter the number of students: " ;
-	cin >> N;
-	cout << "Enter the number of scores for each student: " ;
-	cin >> M;
-
-	// TODO: declare variables for student name, score, sum, average
-
-	for(int i=0; i<N; i++)
-	{
-		// TODO: read student name and initialize sum
-
-		for (int j=0; j<M; j++)
-		{
-			// TODO: read each score and accumulate sum
+	for (int i=0; i<N; i++){
+		cout << "Enter S name ";
+		cin >> sname;
+		for (int j=0; j<M; j++){
+			cout << "Enter score ";
+			cin >> score;
+			total += score;
 		}
-
-		// TODO: calculate average and print name, sum, average
+		double avg = total / (double)M;
+		cout << sname << "\t" << total << "\t" << avg << endl;
 	}
-
 	return 0;
 }
