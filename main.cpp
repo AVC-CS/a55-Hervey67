b4a03a6d98c0262ddf7eba84cb1efa704c1681ab
Hervey67 <herv2007@gmail.com>
2026-03-28 23:03:35 -0700

    Changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -8,12 +8,13 @@ int main()
 	int N, M;
 	cout << "Number of Students and Number of Subjects ";
 	cin >> N >> M;
-		string sname;
-	int score, total = 0;
+	string sname;
+	int score, total;
 
 	for (int i=0; i<N; i++){
 		cout << "Enter S name ";
 		cin >> sname;
+		total = 0;
 		for (int j=0; j<M; j++){
 			cout << "Enter score ";
 			cin >> score;
@@ -21,6 +22,7 @@ int main()
 		}
 		double avg = total / (double)M;
 		cout << sname << "\t" << total << "\t" << avg << endl;
+			
 	}
 	return 0;
 }
